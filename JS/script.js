eca043bf5183ae83c68592542d0dfba45408e239
shubhamseth29 <shubhamseth29007@gmail.com>
2020-08-25 15:20:29 +0530

    Update

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,11 @@ const chatText=document.getElementById("chat-text");
 const mainChat=document.getElementById("main-chat");
 var today = new Date();
 var curtime = today.getHours() + ":" + today.getMinutes();
+const searchInput=document.getElementById("search-input");
+const chatContainer=document.getElementById("chat-container");
+const chatList=document.getElementsByClassName("chat-list");
 
-
+//CODE TO ADD CHAT MESSAGE TO SCREEN FROM CHAT BOX
 chatText.addEventListener("keyup",(e)=>
 {
     if(e.keyCode===13)
@@ -16,51 +19,60 @@ chatText.addEventListener("keyup",(e)=>
         }
         else
         {
-           
-            if(document.getElementById("my-radio").checked)
+           if(document.getElementById("my-radio").checked)
             {
-                console.log("Text entered");
-                var chatRow=document.createElement("div");
-                const text=document.createElement("div");
-                text.appendChild(document.createTextNode(chatText.value));
-                const time=document.createElement("div");
-                time.appendChild(document.createTextNode(curtime));
-                chatRow.appendChild(text);
-                chatRow.appendChild(time);
-                mainChat.appendChild(chatRow);
-                chatRow.classList.add("chat-row","my");
-                text.classList.add("text");
-                time.classList.add("time");
-                chatText.value='';
-                
+                addNewChatMessage("my");
             }
             else if(document.getElementById("other-radio").checked)
             {
-                console.log("Text entered");
-                var chatRow=document.createElement("div");
-                const text=document.createElement("div");
-                text.appendChild(document.createTextNode(chatText.value));
-                const time=document.createElement("div");
-                time.appendChild(document.createTextNode(curtime));
-                chatRow.appendChild(text);
-                chatRow.appendChild(time);
-                mainChat.appendChild(chatRow);
-                chatRow.classList.add("chat-row","other");
-                text.classList.add("text");
-                time.classList.add("time");
-                chatText.value='';
-                
+                addNewChatMessage("other");
             }
             else
             {
                 alert("Please Select Radio Button(My Chat / Other Chat)")
             }
-           
-
-
-
         }
-            
-        
     }
+})
+// FUNCTION TO CREATE NEW DIV AND APPEND
+function addNewChatMessage(textType)
+{
+    console.log("Text entered");
+                var chatRow=document.createElement("div");
+                const text=document.createElement("div");
+                text.appendChild(document.createTextNode(chatText.value));
+                const time=document.createElement("div");
+                time.appendChild(document.createTextNode(curtime));
+                chatRow.appendChild(text);
+                chatRow.appendChild(time);
+                mainChat.appendChild(chatRow);
+                chatRow.classList.add("chat-row",textType);
+                text.classList.add("text");
+                time.classList.add("time");
+                chatText.value='';
+}
+
+//CODE TO SEARCH FOR CONTACT FROM SEARCH BOX
+searchInput.addEventListener('keyup',(e)=>
+{
+       var text=searchInput.value.toLowerCase();
+       var chatTitles=chatContainer.getElementsByClassName("chat-list");
+       Array.from(chatTitles).forEach(function(name)
+       {
+           var chatName=name.textContent;
+           if(chatName.toLowerCase().indexOf(text)!=-1)
+           {
+                name.style.display='block';
+           }
+           else
+           {
+                name.style.display='none';
+           }
+           
+       });
+   
+       
+
+
+
 })
